25746c2d709cd97f0e942b3f7c42c43954a2a2fa
João Perez <peerezjoao1@gmail.com>
2023-01-02 23:45:38 -0300

    Removendo imports não usados

--- forum/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,13 +1,9 @@
 package br.com.alura.forum.service
 
 import br.com.alura.forum.dto.NovoTopicoDto
-import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Topico
-import br.com.alura.forum.model.Usuario
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Retry.Topic
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.PostMapping
-import java.util.*
+
 import kotlin.collections.ArrayList
 
 @Service
